1e145841248b33bb5492cb7c54f1f71171d8d489
ManuelHettich <17690367+ManuelHettich@users.noreply.github.com>
2020-09-08 23:07:21 +0200

    part1: ex 1.6 (unicafe step1)

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({ header }) => (
+    <div>
+      <h2>{ header }</h2>
+    </div>
+)
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = (props) => (
+  <div>
+    good {props.good}<br />
+    neutral {props.neutral}<br />
+    bad {props.bad}<br />
+  </div>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => {
+    setGood(good + 1)
+  }
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const addBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header header={"give feedback"} />
+      <Button handleClick={addGood} text={"good"} />
+      <Button handleClick={addNeutral} text={"neutral"} />
+      <Button handleClick={addBad} text={"bad"} />
+
+      <Header header={"statistics"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
