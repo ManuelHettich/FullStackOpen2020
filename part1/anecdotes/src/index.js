c489b638f207e531ba699f0fc74a8860d8a87f74
ManuelHettich <17690367+ManuelHettich@users.noreply.github.com>
2020-09-09 21:43:21 +0200

    part1: ex 1.14 (anecdotes step3)

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({header}) => (
+  <h2>
+    { header }
+  </h2>
+)
+
+const Anecdote = (props) => (
+  <div>
+    {props.text}<br />
+    has {props.number} votes<br />
+  </div>
+)
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
@@ -11,6 +23,7 @@ const Button = (props) => (
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(6).fill(0))
+  const [mostVoted, setMostVoted] = useState(0)
 
   const selectRandomAnecdote = () => {    
     setSelected(Math.floor(Math.random() * anecdotes.length))
@@ -20,20 +33,26 @@ const App = (props) => {
     const votesCopy = [...votes]
     votesCopy[selected] += 1
     setVotes(votesCopy)
+
+    if (votesCopy[selected] > votesCopy[mostVoted]) {
+      setMostVoted(selected)
+    }
   }
 
   return (
     <div>
-      {props.anecdotes[selected]} <br />
-      has {votes[selected]} votes <br />
+      <Header header={"Anecdote of the day"} />
+      <Anecdote text={props.anecdotes[selected]} number={votes[selected]} />
       <Button handleClick={addVote} text={"vote"} />
       <Button handleClick={selectRandomAnecdote} text={"next anecdote"} />
+      <Header header={"Anecdote with most votes"} />
+      <Anecdote text={props.anecdotes[mostVoted]} number={votes[mostVoted]} />
     </div>
   )
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
+  'If it hurts, do it more often.',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
