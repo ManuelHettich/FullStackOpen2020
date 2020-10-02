866836b77426d20ff763390f79ba2b150b404a6c
ManuelHettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-02 21:09:23 +0200

    part4: ex 4.11

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -14,53 +14,71 @@ beforeEach(async () => {
   }
 })
 
+describe('check database functionality', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('Go To Statement Considered Harmful')
+  })
+
+  test('the unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach(blog => expect(blog.id).toBeDefined())
+  })
+
+  test('successfully create a new blog post', async () => {
+    const newBlog = {
+      title: 'Test Blog Title',
+      author: 'Foo Bar',
+      url: 'localhost',
+      likes: 42
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain('Test Blog Title')
+  })
+
+  test('missing likes property defaults to value 0', async () => {
+    const newBlog = {
+      title: 'Test Blog Title #2',
+      author: 'Foo Baz',
+      url: 'localhost'
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.likes).toBe(0)
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-
-  const titles = response.body.map(r => r.title)
-  expect(titles).toContain('Go To Statement Considered Harmful')
-})
-
-test('the unique identifier property of the blog posts is named id', async () => {
-  const response = await api.get('/api/blogs')
-
-  response.body.forEach(blog => expect(blog.id).toBeDefined())
-})
-
-test('successfully create a new blog post', async () => {
-  const newBlog = {
-    title: 'Test Blog Title',
-    author: 'Foo Bar',
-    url: 'localhost',
-    likes: 42
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
-
-  const titles = blogsAtEnd.map(blog => blog.title)
-  expect(titles).toContain('Test Blog Title')
-})
 
 
 afterAll(() => {
