db8e8c2ee8af68a49b69c04c3c7a9982794792fc
ManuelHettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-02 17:13:03 +0200

    part4: ex 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,6 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accumulator, current) => accumulator + current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  } else {
+    const favoriteBlog = blogs.reduce((favoriteBlog, current) => {
+      return current.likes > favoriteBlog.likes ? current : favoriteBlog
+    }, blogs[0])
+    return {
+      title: favoriteBlog.title,
+      author: favoriteBlog.author,
+      likes: favoriteBlog.likes
+    }
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
