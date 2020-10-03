da826356c90e10db235a5b0bdc92a50aaac03eb8
ManuelHettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-03 21:03:14 +0200

    part4: ex 4.20

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -3,13 +3,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
 
 blogsRouter.get('/', async (_request, response) => {
   const blogs = await Blog.find({})
@@ -19,8 +12,7 @@ blogsRouter.get('/', async (_request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
   const user = await User.findById(decodedToken.id)
 
