ba39fe42c082f638c0f7d72055446d693b33fecc
ManuelHettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-10 18:26:21 +0200

    part5: ex 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,47 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const viewDetails = () => (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <button onClick={toggleVisibility}>hide</button>
+        <br />
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name}
+        <br />
+      </div>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} {viewDetails()}
+    </div>
+  )
+}
 
 export default Blog
