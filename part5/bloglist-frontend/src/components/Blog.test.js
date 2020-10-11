36a27aa3d13ec4215956ced4dd0c9a29d3ef648d
ManuelHettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-11 18:06:46 +0200

    part5: ex 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,36 +1,54 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent, getByText } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content correctly without detailed view', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'Foo',
-    url: 'localhost',
-    likes: 0,
-    user: {
-      name: 'Bar',
-      username: 'bar',
-    },
-  }
+describe('<Blog />', () => {
+  let component
 
-  const mockHandler = jest.fn()
+  beforeEach(() => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'Foo',
+      url: 'localhost',
+      likes: 0,
+      user: {
+        name: 'Bar',
+        username: 'bar',
+      },
+    }
 
-  const component = render(
-    <Blog
-      blog={blog}
-      username="foo"
-      handleLike={mockHandler}
-      handleDelete={mockHandler}
-    />
-  )
+    const mockHandler = jest.fn()
 
-  expect(component.container).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
-  expect(component.container).toHaveTextContent('Foo')
+    component = render(
+      <Blog
+        blog={blog}
+        username="foo"
+        handleLike={mockHandler}
+        handleDelete={mockHandler}
+      />
+    )
+  })
 
-  const span = component.container.querySelector('.blog-show')
-  expect(span).toHaveStyle('display: none')
+  test('renders content correctly without detailed view', () => {
+    expect(component.container).toHaveTextContent(
+      'Component testing is done with react-testing-library'
+    )
+    expect(component.container).toHaveTextContent('Foo')
+
+    const span_show = component.container.querySelector('.blog-show')
+    expect(span_show).toHaveStyle('display: none')
+    const span_hide = component.container.querySelector('.blog-hide')
+    expect(span_hide).not.toHaveStyle('display: none')
+  })
+
+  test('shows detailed view after button click', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    const span_show = component.container.querySelector('.blog-show')
+    expect(span_show).not.toHaveStyle('display: none')
+    const span_hide = component.container.querySelector('.blog-hide')
+    expect(span_hide).toHaveStyle('display: none')
+  })
 })
