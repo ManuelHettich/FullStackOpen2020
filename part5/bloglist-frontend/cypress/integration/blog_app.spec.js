c64c9574c6bf14b407a8edd770518859d1b1581c
ManuelHettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-11 21:28:55 +0200

    part5: ex 5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -19,6 +19,30 @@ describe('Blog app', function () {
     cy.get('#password')
     cy.get('#login-button')
   })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.request('POST', 'http://localhost:3001/api/login', {
+        username: 'foo',
+        password: 'bar',
+      })
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:3001/api/login',
+        form: true,
+        body: {
+          username: 'foo',
+          password: 'foo',
+        },
+        failOnStatusCode: false,
+      }).then(function (response) {
+        expect(response.status).to.eq(401)
+      })
+    })
+  })
 })
 
 describe('when logged in', function () {
